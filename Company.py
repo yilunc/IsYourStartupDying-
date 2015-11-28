6056df10364f55991570f9e5baf0de53ffba2048
Ben Bogusat <benbogusat@Bens-MacBook-Pro.local>
2015-11-27 21:46:10 -0500

    Company Initialization and Getters.

---
+++ Company.py
@@ -0,0 +1,28 @@
+class Company: 
+    def __init__(self, name, status, market, country, funding_value, funding_rounds):
+
+        self._name = name;
+        self._status = status;
+        self._market = market;
+        self._country = country;
+        self._funding_value = funding_value;
+        self._funding_rounds = funding_rounds;
+
+    def getName():
+        return self._name;
+
+    def getStatus():
+        return self._status;
+
+    def getMarket():
+        return self._market;
+
+    def getCountry():
+        return self._country;
+
+    def getFunding_value():
+        return self._funding_value;
+
+    def getFunding_rounds():
+        return self._funding_rounds;
+
