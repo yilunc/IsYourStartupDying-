5093db4e9de97df8444fb360ee87ef2fceed1294
Bbogusat <benbogusat@gmail.com>
2015-11-28 01:38:40 -0500

    init_data parse

--- init_data.py
+++ init_data.py
@@ -1,7 +1,12 @@
 import csv
 
-with open('data.csv', 'rb') as data_file:
-    data_reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-    comp_arr = []
-    for row in spamreader:
+#self, name, status, market, country, funding_value, funding_rounds):
+def parseData_array(csvfile):
+    with open(csvfile, 'rb') as data_file:
+        data_reader = csv.reader(data_file, delimiter=',', quotechar='"')
+        comp_arr = []
+        row = 0
+        for line in data_reader:
+            comp_arr[row] = Company(line[0], line[3], line[1], line[4], line[2], line[7])
 
+    return comp_arr
