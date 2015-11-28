244cbecda9ed2ada6ee2402ecac2bb81a7e4eafb
Bbogusat <benbogusat@gmail.com>
2015-11-28 09:43:16 -0500

    merge

--- init_data.py
+++ init_data.py
@@ -78,23 +78,6 @@ def parseData(csvfile):
             else:
                 market_weights[market] = 2
 
-                country_weights[country] = float(country_totals[country][1])/float(country_totals[country][0]) + 0.5
-            else:
-                country_weights[country] = 2
-
-        for city in city_totals:
-            if (city_totals[city][0] > 0):
-                city_weights[city] = float(city_totals[city][1])/float(city_totals[city][0]) * 10
-            else:
-                city_weights[city] = 2
-
-
-        for market in market_totals:
-            if (float(market_totals[market][0]) > 0):
-                market_weights[market] = float(market_totals[market][1])/float(market_totals[market][0]) * 10
-            else:
-                market_weights[market] = 2
-
     return comp_arr, country_weights, city_weights, market_weights
 
 def company_success(status):
